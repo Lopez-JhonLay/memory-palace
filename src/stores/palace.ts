bb3dcfeca43b710d135a821e68148fdbfd0e03b3
Jhon Lay Lopez <132878493+Lopez-JhonLay@users.noreply.github.com>
2025-04-25 13:02:40 +0800

    create palace store to handle palace state

---
+++ src/stores/palace.ts
@@ -0,0 +1,59 @@
+import { defineStore } from "pinia";
+import { useLocalStorage } from "@/composables/useLocalStorage";
+import type { Palace, Room } from "@/types";
+
+export const usePalaceStore = defineStore("palace", () => {
+  //state
+  const palaces = useLocalStorage<Palace[]>("memory-palaces", []);
+
+  //actions ========== PALACE ==========
+  const addPalace = (palace: Palace) => {
+    palaces.value.push(palace);
+  };
+
+  const updatePalace = (updatedPalace: Palace) => {
+    const index = palaces.value.findIndex((p) => p.id === updatedPalace.id);
+    if (index !== -1) {
+      palaces.value[index] = updatedPalace;
+    }
+  };
+
+  const deletePalace = (palaceId: string) => {
+    palaces.value = palaces.value.filter((p) => p.id !== palaceId);
+  };
+
+  //actions ========== ROOMS ==========
+  const addRoomToPalace = (palaceId: string, room: Room) => {
+    const palace = palaces.value.find((p) => p.id === palaceId);
+    if (palace) {
+      palace.rooms.push(room);
+    }
+  };
+
+  const updateRoomInPalace = (palaceId: string, updatedRoom: Room) => {
+    const palace = palaces.value.find((p) => p.id === palaceId);
+    if (palace) {
+      const index = palace.rooms.findIndex((r) => r.id === updatedRoom.id);
+      if (index !== -1) {
+        palace.rooms[index] = updatedRoom;
+      }
+    }
+  };
+
+  const deleteRoomFromPalace = (palaceId: string, roomId: string) => {
+    const palace = palaces.value.find((p) => p.id === palaceId);
+    if (palace) {
+      palace.rooms = palace.rooms.filter((r) => r.id !== roomId);
+    }
+  };
+
+  return {
+    palaces,
+    addPalace,
+    updatePalace,
+    deletePalace,
+    addRoomToPalace,
+    updateRoomInPalace,
+    deleteRoomFromPalace,
+  };
+});
